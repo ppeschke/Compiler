1c2d5b462fad5099ae1cdcfa69c6a488e8906ac1
Phillip Peschke <ppeschke@austincc.edu>
2019-01-23 13:28:21 -0600

    interpret and compile can now take command line arguments
    - they expect a file name and fall back to test.txt if
      nothing is specified
    
    	modified:   compile.py
    	modified:   interpret.py

--- interpret.py
+++ interpret.py
@@ -2,9 +2,13 @@ from Lexer import *
 from Parser import Parser
 from AST import AST
 from Interpreter import Interpreter
+import sys
 
 def main():
-	text = open('test.txt', 'r').read()
+	if len(sys.argv) == 2:
+		text = open(sys.argv[1], 'r').read()
+	else:
+		text = open('test.txt', 'r').read()
 	if text:
 		lexer = Lexer(text)
 		parser = Parser(lexer)
